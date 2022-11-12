65dbed5fc56673c0bbbcd516be9d4ac6e2093cea
G.Modiriran <Ghodrat.Modiririan@gmail.com>
2022-11-13 00:54:07 +0300

    Admin Activated

--- Shipments/main/models.py
+++ Shipments/main/models.py
@@ -1,3 +1,6 @@
 from django.db import models
-
 # Create your models here.
+class BlogPost(models.Model):
+    title = models.CharField('Title', max_length=120, blank=False, null=False)
+    content = models.TextField('Title', max_length=800, blank=False, null=False)
+    
