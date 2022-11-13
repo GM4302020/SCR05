c3767f5b0e7ca5c941a6b64c3270a57a0ac08e22
G.Modiriran <Ghodrat.Modiririan@gmail.com>
2022-11-13 13:16:28 +0300

    BlogPost worked correctly

--- Shipments/main/admin.py
+++ Shipments/main/admin.py
@@ -1,4 +1,9 @@
 from django.contrib import admin
+from django.contrib.auth.models import Group
 from .models import BlogPost
+
 # Register your models here.
 admin.site.register(BlogPost)
+
+# Unregister your models here.
+admin.site.unregister(Group)
