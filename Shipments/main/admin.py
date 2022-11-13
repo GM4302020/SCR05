4f5b7885dcae5e6c1a449f65def98843b0273069
G.Modiriran <Ghodrat.Modiririan@gmail.com>
2022-11-13 23:05:09 +0300

    admin CMS uploaded and worked

--- Shipments/main/admin.py
+++ Shipments/main/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.contrib.auth.models import Group
-from .models import BlogPost
+from .models import BlogPost, BlogTag
 
 # Register your models here.
 admin.site.register(BlogPost)
+admin.site.register(BlogTag)
 
 # Unregister your models here.
 admin.site.unregister(Group)
--- Shipments/main/models.py
+++ Shipments/main/models.py
@@ -1,10 +1,32 @@
+from django.contrib.auth.models import User
 from django.db import models
-# Create your models here.
-class BlogPost(models.Model):
-    title = models.CharField('Title', max_length=120, blank=False, null=False)
-    content = models.TextField('Content', max_length=800, blank=False, null=False)
+from django.utils import timezone
 
-    def __str__(self) -> str:
+# Create your models here.
+class BlogTag(models.Model):
+    name = models.CharField('Name', max_length=60, blank=False, null=False)
+    slug = models.SlugField('Slug', editable=False, max_length=180, unique=True, default=timezone.now )
+
+    def __str__(self):
+        return self.name
+
+    def save(self, *args, **kwargs):
+        self.slug = str(self.name).strip().replace(' ', '-')
+        super().save(*args, **kwargs)
+
+class BlogPost(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, blank=False, null=False,
+                            verbose_name='Author')
+    title = models.CharField('Title', max_length=120, blank=False, null=False)
+    slug = models.SlugField('Slug', editable=False, max_length=180, unique=True, default=timezone.now )
+    content = models.TextField('Content', max_length=800, blank=False, null=False)
+    banner = models.ImageField('Image', upload_to='images/', blank=True,
+                                null=True, default='images/defaut_image.png')
+    tags = models.ManyToManyField(BlogTag, verbose_name='Tags')
+    
+    def __str__(self):
         return self.title
         
-    
+    def save(self, *args, **kwargs):
+        self.slug = str(self.title).strip().replace(' ', '-')
+        super().save(*args, **kwargs)
